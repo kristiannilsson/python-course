5742542c20c7ecc151e920ff70b7b79770ba4a9a
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2023-10-17 14:32:17 +0200

    lists and loops

--- lektion1_intro/demo/lists.py
+++ lektion1_intro/demo/lists.py
@@ -1,3 +1,35 @@
+#lista
+my_list = ["banana", "apple", "pear", "passionfruit", "grapes"]
+
+#lägga till i lista
+my_list1 = []
+my_list1.append("pineapple")
+print(my_list1)
+
+#modifiera element
+my_list[1] = "pineapple"
+
+my_list.remove("banana")
+my_list.pop(2)
+# remove tar bort värdet, pop tar bort givet index
+my_list = ["22", "1", "9", "70", "55", "grapes"]
+my_list.insert(0, "grapes")
+my_string = "lorem ipsum"
+
+#sortera
+my_list.sort()
+print(my_list)
+
+# längd
+print(len(my_list))
+print(len(my_string))
+
+# slicing [start:stop:step]
+my_list = ["22", "1", "9", "70", "55", "grapes"]
+my_list2 = "apple"
+print(my_list[-1:2:-1])
+print(my_list + my_list2)
+
 """
 1. Skapa en lista
 2. Vad kan finnas i en lista?
--- lektion1_intro/demo/loops.py
+++ lektion1_intro/demo/loops.py
@@ -1,6 +1,24 @@
 nums = [1, 2, 3, 4, 5]
 fruits = ["Apple", "Banana", "Pear", "Pomegranate"]
 
+#loopar igenom listan
+for fruit in fruits:
+    print(fruit)
+
+#loopar igenom 5 gånger
+for i in range(5):
+    print(i)
+counter = 0
+
+#loopar igenom tills counter är 5
+while counter < 5:
+    counter += 1
+    if counter == 2:
+        # loopen breaker (avbryts) när python stöter på break
+        # för att loopen bara ska fortsätta finns continue
+        break
+
+list_comprehension = [num*2 for num in nums]
 """
 1. Loopa igenom listor
 2. In range
