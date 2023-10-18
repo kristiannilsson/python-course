d821be19f2f33052aeb72a05b4811f69cc50dadb
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2023-10-18 11:49:59 +0200

    demo

--- lektion1_intro/demo/strings.py
+++ lektion1_intro/demo/strings.py
@@ -1,11 +1,41 @@
+my_string = "loremipsum"
+
+#loopa igenom sträng
+for char in my_string:
+    print(char)
+
+#printar längden i strängen
+print(len(my_string))
+
+#man kan slica en sträng
+#utelämnade argument default är starten av listan:slutet av listan:1
+print(my_string[2:])
+
+print('hej\"')
+
+my_string = "LoRem ipSum"
+
+print(my_string.lower()) #lowercase
+print(my_string.upper()) #uppercase
+print(my_string.title()) #titlecase
+
+my_fruits = "apple banana grapes"
+my_fruit_list = my_fruits.split()
+
+print(my_fruits.split(" "))
+print(my_fruit_list)
+
+print(", ".join(my_fruit_list))
+
+print(my_fruits.find("pear"))
+
 """
 1. Strängar är listor (len())
 2. Slica en sträng
 3. Sätta ihop strängar
-4. Strängar är immutable
-5. Escape \n
-6. .lower(), .title(), .strip(), .upper()
-7. .split()
-8. .find()
-9. .join()
+4. Escape \n
+5. .lower(), .title(), .strip(), .upper()
+6. .split()
+7. .find()
+8. .join()
 """
--- lektion2_dictionaries_files/dictionaries.py
+++ lektion2_dictionaries_files/dictionaries.py
@@ -1,3 +1,20 @@
+
+
+my_dict = {}
+
+my_dict["brand"] = "Ford"
+my_dict["brand"] = "Saab"
+my_dict["year"] = 1990
+my_dict["miles"] = 1990
+
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+my_dict.update({"price": 2000000})
+my_dict | {"price": 2000000}
+
+print(my_dict.get("color", "red"))
+
 """
 1. Basic syntax för en dictionary, komma åt värden
 2. Skapa en tom dict och sedan lägg till nyckel
