eca5e2cb632426710dc643fd9a1e90940c5b950f
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2023-10-18 13:48:00 +0200

    files

--- lektion2_dictionaries_files/files.py
+++ lektion2_dictionaries_files/files.py
@@ -1,10 +1,28 @@
 import json
 
-f = open("car.json")
-dict = json.load(f)
-dict["color"] = "grey"
-out_file = open("car2.json", "w")
-json.dump(dict, out_file)
+
+def spam():
+    return "Eggs"
+
+
+my_dict = {"my_function": spam}
+# print(my_dict["my_function"]())
+
+f = open("car.json", mode="r", encoding="utf-8")
+contents = json.load(f)
+print(contents)
+new_car = {"brand": "Volvo", "year": 2008}
+contents["cars"] = contents["cars"] + [new_car]
+print(contents)
+f.close()
+
+f = open("car.json", "w", encoding="utf-8")
+json.dump(contents, f)
+
+""" with open("poem.txt") as f:
+    contents = f.read()
+    print(contents) """
+
 
 """
 1. Öppna en ny fil, open() .close()
