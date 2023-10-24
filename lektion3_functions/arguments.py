86796e3420295731c67f592f530a7ae8f819a9aa
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2023-10-24 11:48:35 +0200

    comments fm

--- lektion3_functions/arguments.py
+++ lektion3_functions/arguments.py
@@ -1,3 +1,37 @@
+#positionella argument kommer alltid före namngivna
+# "=" anger ett default- eller fallbackvärde
+
+def my_function(a, b, c, d="a"):
+    print("A", a)
+    print("B", b)
+    print("C", c)
+    print("D", d)
+
+# Print tar oändligt antal argument hur kommer det sig?
+# De använder sig av *args internt
+print(4, 5, 6, 7, 8, 9, 10)
+
+#*args lägger alla överflödiga positionella argument i en read-only lista (tuple)
+#**kwargs lägger alla överflödiga namngivna argument i en dic
+#args och kwargs kan döpas till vad som helst, men konventionen är args och kwargs.
+def args_operator(*args, **kwargs):
+    print(args)
+
+def multiply(*factors):
+    print(factors)
+    product = 1
+    for arg in factors:
+        product *= arg
+    return product
+
+args_operator(1, 2, 3, first_value="a", b="b", c="c")
+
+l = [1, 2, 3, 4]
+dict = {"a": "first_value", "b": "second_value"}
+#
+# *l --> (1, 2, 3, 4)
+
+print(multiply(*l, **dict)) # är ekvivalent med print(multiply(1, 2, 3, 4, a="first_value", b="second_value"))
 """
 1. Positional Arguments
 2. Default arguments
--- lektion3_functions/namespaces.py
+++ lektion3_functions/namespaces.py
@@ -1,6 +1,30 @@
+"""Demo om namespaces. 
+Här lär vi oss om namespaces och dokumentation"""
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+pp.pprint(__builtins__)
+print(dir(__builtins__))
+
+# ligger i det global namespacet
+a = 123
+b = "lorem ipsum"
+
+#locals() printar det nuvarande namespacet. Här är globals samma som locals()
+print(globals() == locals())
+
+def add_two(a):
+    """Function adds two to the argument a"""
+    num_to_add = 2
+    #b tar företräde här
+    #använder du global syftar du på b i det globala namespacet.
+    global b
+    b = 2
+    print(b)
+    
+    return a + num_to_add
+print(add_two(5))
+#print(globals())
 """
 1. Allt är dictionaries!
 2. Varför är print() alltid tillgänglig? dir(__builtins__)
@@ -13,8 +37,10 @@ pp = pprint.PrettyPrinter(indent=4)
 
 def enclosing_function():
   var = "value"
-
+  print(locals())
   def nested_function():
+    #nonlocal syftar på var ett steg upp
+    nonlocal var
     var = "new_value"  
 
   nested_function()
